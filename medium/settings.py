427412915b5fadbce296f4069943e5ba11c0a25e
jitendra <jitendra.pysquad@gmail.com>
2026-02-11 16:21:24 +0530

    fixed no moduled name error

--- medium/settings.py
+++ medium/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from ckeditor_demo.settings import MEDIA_ROOT
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

b9576dbaf284a3c28e1a5f7a842334e2ede91706
jitendra <jitendra.pysquad@gmail.com>
2026-02-11 16:33:42 +0530

    refactor code in setting file

--- medium/settings.py
+++ medium/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-print("------------------------", BASE_DIR)
+# print("------------------------", BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
